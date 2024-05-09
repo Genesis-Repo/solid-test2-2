b2d8dddb01de642e8ddde8b9931336bad910431d
mike-menyala <159994059+mike-menyala@users.noreply.github.com>
2024-05-09 17:26:05 +0800

    commit message for multiple files

--- packages/hardhat/contracts/LoyaltyApp.sol
+++ packages/hardhat/contracts/LoyaltyApp.sol
@@ -7,10 +7,12 @@ contract LoyaltyApp is ERC20 {
     address public owner;
     mapping(address => uint256) public loyaltyPoints;
     mapping(address => uint256) public votingPower;
+    mapping(address => address) public delegatedVotingPower;
 
     event LoyaltyPointsEarned(address indexed user, uint256 points);
     event LoyaltyPointsRedeemed(address indexed user, uint256 points);
     event Voted(address indexed user, uint256 points);
+    event VotingPowerDelegated(address indexed delegator, address indexed delegatee, uint256 points);
 
     modifier onlyOwner() {
         require(msg.sender == owner, "Only contract owner can call this function");
@@ -39,6 +41,15 @@ contract LoyaltyApp is ERC20 {
         votingPower[msg.sender] += points;
         loyaltyPoints[msg.sender] -= points;
         emit Voted(msg.sender, points);
+    }
+
+    function delegateVotingPower(address delegatee) external {
+        require(delegatee != address(0), "Invalid delegatee address");
+        require(msg.sender != delegatee, "Cannot delegate to yourself");
+        require(loyaltyPoints[msg.sender] >= votingPower[msg.sender], "Insufficient voting power to delegate");
+
+        delegatedVotingPower[msg.sender] = delegatee;
+        emit VotingPowerDelegated(msg.sender, delegatee, votingPower[msg.sender]);
     }
 
     function transfer(address recipient, uint256 amount) public override returns (bool) {
